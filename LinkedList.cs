c00a9a6b0396f0260ce02e4b73f9e9a33635b20b
singhtushar689 <singhtushar689@gmail.com>
2023-01-02 13:14:27 +0530

    Simple LinkedList

---
+++ LinkedList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedLists
+{
+    public class LinkedList
+    {
+            public Node head;
+
+            public void Add(int data)
+            {
+                Node node = new Node(data);
+                if (this.head == null)
+                {
+                    this.head = node;
+                }
+                else
+                {
+                    Node temp = head;
+                    while (temp.next != null)
+                    {
+                        temp = temp.next;
+                    }
+                    temp.next = node;
+                }
+                Console.WriteLine("{0} Inserted in linked list ", node);
+
+            }
+            public void Display()
+            {
+                Node temp = this.head;
+                if (temp == null)
+                {
+                    Console.WriteLine("Linkedlist Is Empty");
+                    return;
+                }
+                else
+                {
+                    while (temp != null)
+                    {
+                        Console.WriteLine(temp.data + " ");
+                        temp = temp.next;
+                    }
+                }
+            }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace LinkedLists
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Linkedlist Program");
+            Console.WriteLine("Enter the Options given below \n 1.Create a LinkedList \n 2.Exit");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                 LinkedList linkedList = new LinkedList();
+                 linkedList.Add(56);
+                 linkedList.Add(30);
+                 linkedList.Add(70);
+                 linkedList.Add(80);
+                 linkedList.Display();
+                    break;
+                case 2:
+                    default:
+                    Console.WriteLine("Enter the valid option");
+                    break;
+            }
+            
+        }
+    }
+}
